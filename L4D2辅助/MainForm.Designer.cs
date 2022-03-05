d1e24adc5dd013dededa634b24b9e02edff4ee30
LCiZY <1443351569@qq.com>
2022-03-05 12:45:50 +0800

    init

---
+++ L4D2辅助/MainForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace L4D2辅助
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.checkBox_LD = new System.Windows.Forms.CheckBox();
+            this.checkBox_SK = new System.Windows.Forms.CheckBox();
+            this.checkBox_LT = new System.Windows.Forms.CheckBox();
+            this.label_LT = new System.Windows.Forms.Label();
+            this.label_SK = new System.Windows.Forms.Label();
+            this.label_LD = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // 
+            // checkBox_LT
+            // 
+            this.checkBox_LT.AutoSize = true;
+            this.checkBox_LT.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox_LT.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox_LT.ForeColor = System.Drawing.Color.Black;
+            this.checkBox_LT.Location = new System.Drawing.Point(49, 29);
+            this.checkBox_LT.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox_LT.Name = "checkBox_LT";
+            this.checkBox_LT.Size = new System.Drawing.Size(74, 28);
+            this.checkBox_LT.TabIndex = 5;
+            this.checkBox_LT.Text = "连跳";
+            this.checkBox_LT.UseVisualStyleBackColor = false;
+            this.checkBox_LT.CheckedChanged += new System.EventHandler(this.findL4D2PID);
+            // 
+            // checkBox_SK
+            // 
+            this.checkBox_SK.AutoSize = true;
+            this.checkBox_SK.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox_SK.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox_SK.ForeColor = System.Drawing.Color.Black;
+            this.checkBox_SK.Location = new System.Drawing.Point(205, 29);
+            this.checkBox_SK.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox_SK.Name = "checkBox_SK";
+            this.checkBox_SK.Size = new System.Drawing.Size(74, 28);
+            this.checkBox_SK.TabIndex = 6;
+            this.checkBox_SK.Text = "速砍";
+            this.checkBox_SK.UseVisualStyleBackColor = false;
+            // 
+            // checkBox_LD
+            // 
+            this.checkBox_LD.AutoSize = true;
+            this.checkBox_LD.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox_LD.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox_LD.ForeColor = System.Drawing.Color.Black;
+            this.checkBox_LD.Location = new System.Drawing.Point(372, 29);
+            this.checkBox_LD.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox_LD.Name = "checkBox_LD";
+            this.checkBox_LD.Size = new System.Drawing.Size(74, 28);
+            this.checkBox_LD.TabIndex = 7;
+            this.checkBox_LD.Text = "连点";
+            this.checkBox_LD.UseVisualStyleBackColor = false;
+            // 
+            // label_LT
+            // 
+            this.label_LT.AutoSize = true;
+            this.label_LT.BackColor = System.Drawing.Color.Transparent;
+            this.label_LT.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_LT.ForeColor = System.Drawing.Color.Black;
+            this.label_LT.Location = new System.Drawing.Point(49, 62);
+            this.label_LT.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_LT.Name = "label_LT";
+            this.label_LT.Size = new System.Drawing.Size(111, 29);
+            this.label_LT.TabIndex = 10;
+            this.label_LT.Text = "(按住空格)";
+            // 
+            // label_SK
+            // 
+            this.label_SK.AutoSize = true;
+            this.label_SK.BackColor = System.Drawing.Color.Transparent;
+            this.label_SK.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_SK.ForeColor = System.Drawing.Color.Black;
+            this.label_SK.Location = new System.Drawing.Point(205, 62);
+            this.label_SK.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_SK.Name = "label_SK";
+            this.label_SK.Size = new System.Drawing.Size(83, 29);
+            this.label_SK.TabIndex = 9;
+            this.label_SK.Text = "(按住X)";
+            // 
+            // label_LD
+            // 
+            this.label_LD.AutoSize = true;
+            this.label_LD.BackColor = System.Drawing.Color.Transparent;
+            this.label_LD.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_LD.ForeColor = System.Drawing.Color.Black;
+            this.label_LD.Location = new System.Drawing.Point(372, 62);
+            this.label_LD.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_LD.Name = "label_LD";
+            this.label_LD.Size = new System.Drawing.Size(83, 29);
+            this.label_LD.TabIndex = 8;
+            this.label_LD.Text = "(按住T)";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(512, 274);
+            this.Controls.Add(this.label_LT);
+            this.Controls.Add(this.label_SK);
+            this.Controls.Add(this.label_LD);
+            this.Controls.Add(this.checkBox_LD);
+            this.Controls.Add(this.checkBox_SK);
+            this.Controls.Add(this.checkBox_LT);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "L4D2辅助";
+            this.Load += new System.EventHandler(this.Init);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox_LD;
+        private System.Windows.Forms.CheckBox checkBox_SK;
+        private System.Windows.Forms.CheckBox checkBox_LT;
+        private System.Windows.Forms.Label label_LT;
+        private System.Windows.Forms.Label label_SK;
+        private System.Windows.Forms.Label label_LD;
+    }
+}
+
---
+++ L4D2辅助/MainForm.cs
@@ -0,0 +1,197 @@
+﻿using Memory;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using MessageBox = System.Windows.Forms.MessageBox;
+
+namespace L4D2辅助
+{
+    public partial class MainForm : Form
+    {
+
+        public Mem m = new Mem();
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vKey);
+
+        [DllImport("user32")]
+        private static extern int mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        //移动鼠标 
+        const int MOUSEEVENTF_MOVE = 0x0001;
+        //模拟鼠标左键按下 
+        const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        //模拟鼠标左键抬起 
+        const int MOUSEEVENTF_LEFTUP = 0x0004;
+        //模拟鼠标右键按下 
+        const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        //模拟鼠标右键抬起 
+        const int MOUSEEVENTF_RIGHTUP = 0x0010;
+        //模拟鼠标中键按下 
+        const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        //模拟鼠标中键抬起 
+        const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+        //标示是否采用绝对坐标 
+        const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+        //模拟鼠标滚轮滚动操作，必须配合dwData参数
+        const int MOUSEEVENTF_WHEEL = 0x0800;
+
+        string JUMP = "";//内存位置，形如 client.dll+0x756DF0
+        string AIR = "";//内存位置，形如 client.dll+0x6E0008
+
+        public MainForm()
+        {
+            LoadAddress();
+            InitializeComponent();
+        }
+
+        private void LoadAddress()
+        {
+            var path = @"./address.ini";
+
+            if (!File.Exists(path)){  // 文件不存在，写入规则
+                var file = File.Create(path);
+                var content = "###地址值配置文件###\r\n###只在版本更新时需要更改配置文件的内容(每次版本更新游戏程序的地址值会变)\r\n###得到并修改以下两个十六进制地址值（地址值通过CE得到，方法如下：\r\n###JUMP: 这个地址内储存的值的特征是：初始值是4，在游戏的控制台输入+jump变成5，输入-jump变成4，跳的时候会在4和5之间变化\r\n###AIR：这个地址内储存的值特征是：初始是0（不跳跃的时候）。跳跃在空中时增大（一个大数），落地又变成0，走路的时候不会变。）\r\n\r\nJUMP=client.dll+0x757DF0\r\nAIR=client.dll+0x6E1008";
+                var bytes = System.Text.Encoding.Default.GetBytes(content);
+                file.Write(bytes, 0, bytes.Length);
+                file.Close();
+            }
+
+            string[] lines = File.ReadAllLines(path, Encoding.UTF8);
+            foreach (string line in lines)
+            {
+                var rLine = line.Trim();
+                if (rLine.StartsWith("#")) continue;
+                string[] keyAndVal = rLine.Split('=');
+                if (keyAndVal.Length == 2)
+                {
+                    var key = keyAndVal[0].Trim();
+                    var val = keyAndVal[1].Trim();
+                    if (key == "JUMP")
+                    {
+                        JUMP = val;
+                    }
+                    if (key == "AIR")
+                    {
+                        AIR = val;
+                    }
+                }
+            }
+        }
+
+        private void Init(object sender, EventArgs e)
+        {
+            Thread BH = new Thread(BHOP) { IsBackground = true };
+            BH.Start();
+            Thread SK = new Thread(SUKAN) { IsBackground = true };
+            SK.Start();
+            Thread LD = new Thread(LIANDIAN) { IsBackground = true };
+            LD.Start();
+        }
+
+        private int left4dead2PID = 0;
+        void findL4D2PID(object sender, EventArgs e)
+        {
+            if (checkBox_LT.Checked)
+            {
+                int PID = m.GetProcIdFromName("left4dead2");
+                if (PID > 0)
+                    m.OpenProcess(PID);
+                left4dead2PID = PID;
+                if(PID <= 0)
+                    if (MessageBox.Show("请进入游戏后再勾选", "提示", MessageBoxButtons.OK) == DialogResult.OK)
+                        checkBox_LT.Checked = false;
+            }
+            else
+            {
+                if(left4dead2PID != 0)
+                    m.CloseProcess();
+                left4dead2PID = 0;
+            }
+        }
+
+        int lastResult;
+        long groundTimeStamp;
+        long airTimeStamp;
+        void BHOP()
+        {
+            while (true)
+            {
+                if (checkBox_LT.Checked)
+                {
+                    if(left4dead2PID > 0 && !(JUMP.Equals("") || AIR.Equals("")))
+                    {
+                        if (GetAsyncKeyState(Keys.Space) < 0)
+                        {
+                            int result = m.ReadInt(AIR); 
+                            if (result == 0)
+                            {
+                                m.WriteMemory(JUMP, "int", "5"); //4→5是起跳
+                                Thread.Sleep(1);
+                                m.WriteMemory(JUMP, "int", "4");
+                            }
+
+
+                            ////debug: 接触地面的时间
+                            //result = m.ReadInt(AIR);
+                            //if(result == 0 && lastResult != 0)//落地
+                            //{
+                            //    groundTimeStamp = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                            //}
+                            //if(result != 0 && lastResult == 0)//跳起
+                            //{
+                            //    airTimeStamp = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                            //    Console.WriteLine(airTimeStamp - groundTimeStamp);  
+                            //}
+                            //lastResult = result;
+
+                        }
+                    }
+                }
+                if (left4dead2PID <= 0 || !checkBox_LT.Checked || GetAsyncKeyState(Keys.Space) >= 0) //按下空格的时候不挂起进程，挂起进程会导致不能及时执行此函数的代码而造成起跳时机不适合
+                    Thread.Sleep(1); //在不按下空格时挂起防止CPU使用率过高
+            }
+        }
+        void SUKAN()
+        {
+            while (true)
+            {
+                if (checkBox_SK.Checked)
+                {
+                    if (GetAsyncKeyState(Keys.X) < 0)
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        Thread.Sleep(10);
+                        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                        Thread.Sleep(35);
+                        mouse_event(MOUSEEVENTF_WHEEL, 0, 0, 1, 0);
+                        Thread.Sleep(35);
+                        mouse_event(MOUSEEVENTF_WHEEL, 0, 0, -1, 0);
+                        Thread.Sleep(500);
+                    }
+                }
+                Thread.Sleep(1);
+            }
+        }
+        void LIANDIAN()
+        {
+            while (true)
+            {
+                if (checkBox_LD.Checked)
+                {
+                    if (GetAsyncKeyState(Keys.T) < 0)
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        Thread.Sleep(50);
+                        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                        Thread.Sleep(50);
+                    }
+                }
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
